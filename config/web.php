83dc4bf0b10bbcbca95edb063bd0c8ffb40c17cc
nick <nick_php@163.com>
2018-12-25 15:57:06 +0800

    :memo: add

--- config/web.php
+++ config/web.php
@@ -14,6 +14,17 @@ $config = [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'admin'=>[
+            'class'=>'app\modules\admin\AdminModule',
+        ],
+        'api'=>[
+            'class'=>'app\modules\api\ApiModule',
+        ],
+        'jobs'=>[
+            'class'=>'app\modules\jobs\JobsModule',
+        ]
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
---
+++ helpers/functions.php
@@ -0,0 +1,6 @@
+<?php  
+
+function demo() 
+{
+    echo 'demo';
+}
