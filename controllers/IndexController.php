268d71402372d8d39bca33a1348f28ca63d1c707
nick <nick_php@163.com>
2019-01-06 10:15:00 +0800

    ::construction: update

---
+++ controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class IndexController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'index->index';
+    }
+}
