4bb46575c5d36a6edfc3ac6a6c6269e1699e2ea1
nick <nick_php@163.com>
2019-01-16 22:37:28 +0800

    :pencil2: add init beforeAction afterAction

--- modules/admin/controllers/IndexController.php
+++ modules/admin/controllers/IndexController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace app\modules\admin\controllers;
 
@@ -6,9 +6,38 @@ use yii\web\Controller;
 
 class IndexController extends Controller
 {
+    public $defaultAction = 'home';
+
+    public function actionHome()
+    {
+        echo __METHOD__;
+        echo '<br>';
+    }
+
+    public function init()
+    {
+        echo __METHOD__;
+        echo '<br>';
+    }
+
     public function actionIndex()
     {
-        return 'admin index index';
+        echo __METHOD__;
+        echo '<br>';
+    }
+
+    public function beforeAction($action)
+    {
+        echo __METHOD__;
+        echo '<br>';
+        return true;
+    }
+
+    public function afterAction($action, $result)
+    {
+        echo __METHOD__;
+        echo '<br>';
+        return true;
     }
 }
 
