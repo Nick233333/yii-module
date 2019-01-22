de0e5d85c513c44a5b9fea16c539602cb9b86c7c
nick <nick_php@163.com>
2019-01-22 21:58:58 +0800

    :sparkles: add csrf token

--- modules/admin/controllers/IndexController.php
+++ modules/admin/controllers/IndexController.php
@@ -3,41 +3,48 @@
 namespace app\modules\admin\controllers;
 
 use yii\web\Controller;
+use Yii;
 
 class IndexController extends Controller
 {
     public $defaultAction = 'home';
+    public $enableCsrfValidation = false;
 
-    public function actionHome()
-    {
-        echo __METHOD__;
-        echo '<br>';
-    }
+    // public function actionHome()
+    // {
+    //     echo __METHOD__;
+    //     echo '<br>';
+    // }
+    //
+    // public function init()
+    // {
+    //     echo __METHOD__;
+    //     echo '<br>';
+    // }
+    //
+    // public function actionIndex()
+    // {
+    //     echo __METHOD__;
+    //     echo '<br>';
+    // }
+    //
+    // public function beforeAction($action)
+    // {
+    //     echo __METHOD__;
+    //     echo '<br>';
+    //     return true;
+    // }
+    //
+    // public function afterAction($action, $result)
+    // {
+    //     echo __METHOD__;
+    //     echo '<br>';
+    //     return true;
+    // }
 
-    public function init()
+    public function actionTest()
     {
-        echo __METHOD__;
-        echo '<br>';
-    }
-
-    public function actionIndex()
-    {
-        echo __METHOD__;
-        echo '<br>';
-    }
-
-    public function beforeAction($action)
-    {
-        echo __METHOD__;
-        echo '<br>';
-        return true;
-    }
-
-    public function afterAction($action, $result)
-    {
-        echo __METHOD__;
-        echo '<br>';
-        return true;
+        return Yii::$app->request->post('name', '111');
     }
 }
 
