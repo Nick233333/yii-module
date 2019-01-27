925f2c847fa02857c5cf6e5215a8d889137a3ae8
nick <nick_php@163.com>
2019-01-27 22:03:45 +0800

    :sparkles: add actions

--- actions/TestAction.php
+++ actions/TestAction.php
@@ -1,7 +1,28 @@
 <?php
 
+namespace app\actions;
 
-class TestAction
+use yii\base\Action;
+
+class TestAction extends Action
 {
+    public $param1 = null;
+    public $param2 = null;
+    public $param3 = null;
 
+    public function run()
+    {
+        echo "test run param1: {$this->param1} param2: {$this->param2} param3: {$this->param3}";
+    }
+
+    public function beforeRun()
+    {
+        echo 'beforeRun';
+        return true;
+    }
+
+    public function afterRun()
+    {
+        echo 'afterRun';
+    }
 }
--- modules/admin/controllers/DemoController.php
+++ modules/admin/controllers/DemoController.php
@@ -1,10 +1,37 @@
 <?php
 
-
 namespace app\modules\admin\controllers;
 
+use yii\web\Controller;
 
-class DemoController
+class DemoController extends Controller
 {
 
+    public function actions()
+    {
+        return [
+            'test-info' => [
+                'class' => 'app\actions\TestAction',
+                'param1' => 'hello',
+                'param2' => 'world',
+                'param3' => '!!!',
+            ],
+            'user' => [
+                'class' => 'app\actions\TestAction',
+                'param1' => 'hello',
+                'param2' => 'world',
+                'param3' => '!!!',
+            ],
+        ];
+    }
+
+    public function actionTestInfo()
+    {
+        echo 'test-info';
+    }
+
+    public function actionUser()
+    {
+        echo 'user';
+    }
 }
